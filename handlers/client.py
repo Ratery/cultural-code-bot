4a1562050f1c2be97a31db1abe44ec42b114a235
py-cyber <vinogradovdima881@gmail.com>
2023-01-22 12:02:30 +0300

    -#

--- handlers/client.py
+++ handlers/client.py
@@ -3,17 +3,14 @@ from main import dp, bot
 from keyboards import kb_client
 
 
-# @dp.message_handler(commands='start')
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!", reply_markup=kb_client)
 
 
-# @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
 
 
-# @dp.message_handler(commands=['1'])
 async def process_command_1(message: types.Message):
     await message.reply("Первая инлайн кнопка", )
 
@@ -24,7 +21,12 @@ async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')"""
 
 
+async def empty(message: types.Message):
+    await message.answer('Я не понимаю')
+
+
 def register_client_handlers(dp: Dispatcher):
     dp.register_message_handler(process_start_command, commands=['start'])
     dp.register_message_handler(process_help_command, commands=['help'])
     dp.register_message_handler(process_command_1, commands=['1'])
+    dp.register_message_handler(empty)
