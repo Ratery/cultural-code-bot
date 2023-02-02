cda2b5b55058feeb5c79adaf5648932e07fbd31d
Ratery <nik.bylnov@mail.ru>
2023-02-02 22:51:34 +0300

    Add comment to test results

--- handlers/quiz.py
+++ handlers/quiz.py
@@ -30,12 +30,62 @@ async def send_question(message: types.Message, questions_order: List[int], numb
     )
     return await message.answer_poll(
         type="quiz",
-        question=f"[{number + 1}/{len(questions_order)}]{question['question']}",
+        question=f"[{number + 1}/{len(questions_order)}] {question['question']}",
         options=[options[i] for i in options_order],
         correct_option_id=options_order.index(question['correct_option'] - 1),
         explanation=question.get('explanation'),
         is_anonymous=False,
         reply_markup=keyboard
+    )
+
+
+async def send_results(
+    message: types.Message,
+    title: str,
+    correct_answers: int,
+    questions_passed: int,
+) -> types.Message:
+    if questions_passed < 5:
+        comment = "Рекомендуем пройти тест ещё раз и проверить свои знания."
+    else:
+        percentage = correct_answers / questions_passed
+        if percentage <= 0.2:
+            comment = clean_msg(
+                """
+                Тебе предстоит ещё многое узнать о правилах этикета.
+                Подпишись на наш <a href="https://t.me/cultural_code_lyceum">канал</a> и откроешь для себя много нового.
+                """
+            )
+        elif percentage <= 0.5:
+            comment = clean_msg(
+                """
+                Ты знаешь базовые правила этикета, молодец. Но никогда не помешает знать чуть больше.
+                Подпишись на наш <a href="https://t.me/cultural_code_lyceum">канал</a>, чтобы улучшить свой результат. 
+                """
+            )
+        elif percentage <= 0.8:
+            comment = clean_msg(
+                """
+                Ты, вероятно, знаешь уже многое о культуре и правилах этикета, но совершенствоваться всегда полезно.
+                В нашем <a href="https://t.me/cultural_code_lyceum">канале</a> ты сможешь узнать ещё больше.
+                """
+            )
+        else:
+            comment = clean_msg(
+                """
+                Ты знаешь всё об этикете. Достойно уважения. Но нет предела совершенству.
+                В нашем <a href="https://t.me/cultural_code_lyceum">канале</a> мы регулярно рассказываем что-то интересное.
+                """
+            )
+    main_text = clean_msg(
+        f"""
+        📙 <b>{title}</b>
+        <b>Твой результат:</b> {correct_answers}/{questions_passed}
+        """
+    )
+    return await message.answer(
+        text=f"{main_text}\n{comment}",
+        reply_markup=main_menu_keyboard
     )
 
 
@@ -66,14 +116,11 @@ async def cmd_cancel(message: types.Message, state: FSMContext) -> None:
     correct_answers: int = state_data['correct_answers']
     questions_passed = state_data['question_number'] + 1
     await state.clear()
-    await message.answer(
-        text=clean_msg(
-            f"""
-            📙 <b>Тест прерван</b>
-            <b>Твой результат:</b> {correct_answers}/{questions_passed}
-            """
-        ),
-        reply_markup=main_menu_keyboard
+    await send_results(
+        message=message,
+        title="Тест прерван",
+        correct_answers=correct_answers,
+        questions_passed=questions_passed
     )
 
 
@@ -93,14 +140,11 @@ async def handle_poll_answer(answer: types.PollAnswer, state: FSMContext) -> Non
         await message.answer("❌ Неправильно! В следующий раз всё получится!")
     if question_number == questions_count:
         await state.clear()
-        await message.answer(
-            text=clean_msg(
-                f"""
-                📙 <b>Тест пройден</b>
-                <b>Твой результат:</b> {correct_answers}/{questions_count}
-                """
-            ),
-            reply_markup=main_menu_keyboard
+        await send_results(
+            message=message,
+            title="Тест пройден",
+            correct_answers=correct_answers,
+            questions_passed=questions_count
         )
     else:
         poll = await send_question(
