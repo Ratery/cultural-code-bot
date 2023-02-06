75c757bb9cfa5e7d66838074c46fa89d3fa7dbb6
Ratery <nik.bylnov@mail.ru>
2023-02-06 22:07:32 +0300

    Add channel link to start command

--- handlers/start.py
+++ handlers/start.py
@@ -15,7 +15,11 @@ async def process_start_command(message: types.Message) -> None:
             f"""
             👋 Привет, <b>{message.from_user.full_name}</b>!
             Используй кнопки под клавиатурой для навигации.
+            /help — Помощь
+            
+            ⭐️ Подпишитесь на наш <a href="https://t.me/cultural_code_lyceum">основной канал</a>.
             """
         ),
-        reply_markup=main_menu_keyboard
+        reply_markup=main_menu_keyboard,
+        disable_web_page_preview=True
     )
