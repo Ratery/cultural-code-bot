348b84e3efd90fe95125d8a3ee7a14148bcd7dad
Ratery <72614905+Ratery@users.noreply.github.com>
2025-03-02 18:19:30 +0300

    Upgrade to aiogram `v3.18.0`

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import logging
 import importlib
 
 from aiogram import Bot, Dispatcher, F
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config_reader import config
@@ -21,14 +23,16 @@ def load_handlers(dp: Dispatcher) -> None:
 
 
 async def main() -> None:
-    logging.basicConfig(level=logging.DEBUG)
-    bot = Bot(token=config.bot_token.get_secret_value(), parse_mode='HTML')
-
+    bot = Bot(
+        token=config.bot_token.get_secret_value(),
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
     dp = Dispatcher(storage=MemoryStorage())
     dp.message.filter(F.chat.type == 'private')
     dp.message.middleware(ThrottlingMiddleware())
     dp.callback_query.middleware(ThrottlingMiddleware())
 
+    logging.basicConfig(level=logging.DEBUG)
     logging.log(
         level=logging.INFO,
         msg=f"Bot running as @{(await bot.get_me()).username}"
